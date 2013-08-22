ce3c82a2947f4b893693b761d5b129cf56c11949
cssmagic <cssmagic.cn@gmail.com>
2013-08-22 15:12:06 +0800

    Update.

--- src/tiny.js
+++ src/tiny.js
@@ -1,37 +1,100 @@
-/*! Tiny.js v0.1.0 **/
-var $ = function(s){return document.querySelectorAll(s);};
-$.id = function(s){return document.getElementById(s);};
-$.cls = function(s){return document.getElementsByClassName(s);};
-$.tag = function(s,eWrapper){return (eWrapper || document).getElementsByTagName(s);};
-$.crE = function(s){return document.createElement(s);};
-$.crT = function(s){return document.createTextNode(s);};
-$.insBfr = function(eW,e){eW.insertBefore(e,eW.firstChild);};
-$.rmv = function (e) {e.parentNode.removeChild(e);};
-$.getT = function(e){return e.firstChild.data;};
-$.setT = function(e,s){e.firstChild.data=s;};
-$.hide = function(e){e.style.visibility='hidden';};
-$.show = function(e){e.style.visibility='visible';};
-$.off = function(e){e.style.display='none';};
-$.on = function(e){e.style.display='';};
-$.style = function(e,p,v){
-	if (v) {
-		e.style[p] = v;
-	} else {
-		e.style.cssText = p;
+/*! Tiny.js v0.2.0 **/
+
+//namespace
+var $ = function (s) {
+	return document.querySelectorAll(s);
+};
+
+//utilties - array-like
+$.each = function (a, fn, context) {
+	for (var i = 0, l = a.length; i < l; ++i) {
+		fn.call(context || window, a[i], i, a);
 	}
 };
-$.addEv = function(e,sEv,fn){e.addEventListener(sEv,fn,false);};
-$.rmvEv = function(e,sEv,fn){e.removeEventListener(sEv,fn,false);};
-$.each = function (a,fn){for(var i=0,l=a.length;i<l;++i){fn(a[i]);}};
-$.hasT = function(so,s){return so.indexOf(s)>-1;};
-$.hasCls = function(so,s){return $.hasT(' '+so+' ',' '+s+' ');};
-$.addCls = function(e,s){var so=e.className;if(!$.hasCls(so,s))e.className+=(' '+s);};
-$.rmvCls = function(e,s){var so=e.className;if($.hasCls(so,s))e.className=(' '+so+' ').replace(' '+s+' ',' ').trim();};
+
+//utilties - str
+$.strstr = function (so, s) {
+	return so.indexOf(s) > -1;
+};
+
+//dom query
+$.id = function (s) {
+	return document.getElementById(s);
+};
+$.cls = function (s, eWrapper) {
+	return (eWrapper || document).getElementsByClassName(s);
+};
+$.tag = function (s, eWrapper) {
+	return (eWrapper || document).getElementsByTagName(s);
+};
+
+//creat
+$.crElem = function (s) {
+	return document.createElement(s);
+};
+$.crText = function (s) {
+	return document.createTextNode(s);
+};
+
+//mod dom
+$.insBefore = function (eWrapper, e) {
+	eWrapper.insertBefore(e, eWrapper.firstChild);
+};
+$.remove = function (e) {
+	e.parentNode.removeChild(e);
+};
+
+//classname
+$.hasClass = function (e, s) {
+	return $.strstr(' ' + e.className + ' ', ' ' + s + ' ');
+};
+$.addClass = function (e, s) {
+	var so = e.className;
+	if (!$.hasClass(so, s)) {
+		e.className += (' ' + s);
+	}
+};
+$.removeClass = function (e,s) {
+	var so = e.className;
+	if ($.hasClass(so, s)) {
+		e.className = (' ' + so + ' ').replace(' ' + s + ' ', ' ').trim();
+	}
+};
+
+//style
+$.visible = function (e, b) {
+	var val = b ? 'visible' : 'hidden';
+	e.style.visibility = val;
+};
+$.hide = function (e) {
+	e.style.display = 'none';
+};
+$.show = function (e) {
+	e.style.display = '';
+};
+$.setStyle = function (e, prop, val) {
+	if (v) {
+		e.style[prop] = val;
+	} else {
+		e.style.cssText = prop;
+	}
+};
 $.css = function (s) {
-	var css = $.crE('style');
-	css.innerHTML = s;
-	console.log(css);
-	$.tag('head')[0].appendChild(css);
+	var e = $.crElem('style');
+	var cssText = s || $.cssText;
+	if (cssText) {
+		e.innerHTML = cssText;
+		//console.log(css);
+		$.tag('head')[0].appendChild(e);
+	}
 };
 $.cssText = '';
+
+//event
+$.on = function (e, sEvent, fn) {
+	e.addEventListener(sEvent, fn, false);
+};
+$.off = function (e, sEvent, fn) {
+	e.removeEventListener(sEvent, fn, false);
+};
 
